ec14a4dbd5b97bebcdab49bad1da56d69a15dbc0
Yuliya Sinitskaya <baukun.yuliya@mail.ru>
2023-03-22 00:57:25 +0300

    feat: добавить поп-ап форму

---
+++ script.js
@@ -0,0 +1,25 @@
+const nameInput = document.querySelector('.popup__input_type_name');
+const infoInput = document.querySelector('.popup__input_type_info');
+const editButton = document.querySelector('.btn_action_profile-edit');
+const popup = document.querySelector('.popup');
+const profileName = popup.querySelector('.profile__name');
+const profileDescription = popup.querySelector('profile__description');
+const closeButton = document.querySelector('.btn_action_close-popup');
+
+function openPopup() {
+  popup.classList.add('popup__opened');
+  nameInput.value = profileName.textContent;
+  infoInput.value = profileDescription.textContent;
+};
+
+editButton.addEventListener('click', function() {
+  openPopup(popup);
+});
+
+function closePopup() {
+  popup.classList.remove('popup__opened');
+};
+
+closeButton.addEventListener('click', function() {
+  closePopup(popup);
+});
