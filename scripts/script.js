7116089f90eae3a4ab88a9a31c3dea104681d304
Yuliya Sinitskaya <baukun.yuliya@mail.ru>
2023-05-19 02:59:05 +0300

    feat: добавить попап с изображением

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,22 @@
 const nameInput = document.querySelector('.popup__input_type_name');
 const infoInput = document.querySelector('.popup__input_type_info');
 const editBtn = document.querySelector('.btn_action_profile-edit');
-const editProfilePopup = document.querySelector('.popup_type_profile-edit');
+const editProfilePopup = document.querySelector('.popup__profile-edit');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 const profilePopupCloseBtn = document.querySelector('.btn_action_close-popup-profile');
 const saveButton = document.querySelector('.btn_action_save-popup');
 const profileSaveButton = document.querySelector('.btn_action_save-popup-profile');
-const addPlacePopup = document.querySelector('.popup_type_add-place');
+const addPlacePopup = document.querySelector('.popup__add-place');
 const placeInput = document.querySelector('.popup__input_type_place');
 const linkInput = document.querySelector('.popup__input_type_link');
 const addPlaceBtn = document.querySelector('.btn_action_add-place');
-const placePopupCloseBtn = document.querySelector('.btn_action_close-popup-place');
+const addPlacePopupCloseBtn = document.querySelector('.btn_action_close-popup-place');
 const placeSaveButton = document.querySelector('.btn_action_save-popup-place');
+const imgPopup = document.querySelector('.popup__place');
+const placePopupCloseBtn = document.querySelector('.btn_action_close-popup-img');
+const cardsContainer = document.querySelector('.places-grid__container');
+const cardTemplate = document.querySelector('#card-template');
 
 function openPopup(popup) {
   popup.classList.add('popup__opened');
@@ -26,9 +30,14 @@ profilePopupCloseBtn.addEventListener('click', (evt) => {
   closePopup(editProfilePopup);
 });
 
-placePopupCloseBtn.addEventListener('click', (evt) => {
+addPlacePopupCloseBtn.addEventListener('click', (evt) => {
   closePopup(addPlacePopup);
 });
+
+placePopupCloseBtn.addEventListener('click', (evt) => {
+  closePopup(imgPopup);
+});
+
 
 //попап редактирования профиля
 editBtn.addEventListener('click', () => {
@@ -48,7 +57,7 @@ profileSaveButton.addEventListener('click', (evt) => {
   handleProfileFormSubmit(evt);
 });
 
-editProfilePopup.addEventListener('submit',handleProfileFormSubmit);
+editProfilePopup.addEventListener('submit', handleProfileFormSubmit);
 
 //попап добавления карточки
 addPlaceBtn.addEventListener('click', () => {
@@ -59,52 +68,51 @@ function handlePlaceFormSubmit(evt) {
   evt.preventDefault();
   renderCard(placeInput.value, linkInput.value);
   closePopup(addPlacePopup);
-  evt.target.reset();
 }
 
 placeSaveButton.addEventListener('click', (evt) => {
   handlePlaceFormSubmit(evt);
 });
 
-//Добавление карточек
+placeSaveButton.addEventListener('submit', handlePlaceFormSubmit);
+
+//Карточки для добавления
 
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Баган, Мьянма',
+    link: 'https://images.unsplash.com/photo-1556199859-0a5f16d962a8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Ангкор-Ват, Камбоджа',
+    link: 'https://images.unsplash.com/photo-1644651051345-d642fc1fcbff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=848&q=80'
   },
   {
     name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    link: 'https://images.unsplash.com/photo-1552735855-557bdba3961a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=689&q=80'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://images.unsplash.com/photo-1568028476727-0c86534220fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1631&q=80'
+  },
+  {
+    name: 'Долина Йосемити, США',
+    link: 'https://images.unsplash.com/photo-1569597408915-cb395eee98b0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'
+  },
+  {
+    name: 'Река Ли, Китай',
+    link: 'https://images.unsplash.com/photo-1556880003-4fcd06418af3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'
   }
 ];
-
-
-const cardsContainer = document.querySelector('.places-grid__container');
-const cardTemplate = document.querySelector('#card-template');
 
 //Создание карточки
 const createCard = (placeName, imageLink) => {
   const template = cardTemplate.content.querySelector('.card').cloneNode(true);
-  template.querySelector('.card__img').src = imageLink;
+  const imgPlace = template.querySelector('.card__img');
+  imgPlace.src = imageLink;
   template.querySelector('.card__heading').textContent = placeName;
-  template.querySelector('.card__img').alt = placeName;
+  imgPlace.alt = placeName;
+  imgPlace.addEventListener('click', () => openImgPopup(placeName, imageLink));
 
 //Удаление карточки
   const deleteBtn = template.querySelector('.btn_action_delete-card');
@@ -129,3 +137,10 @@ const renderCard = (placeName, imageLink) => {
 };
 
 cardsContainer.prepend(...templatesList);
+
+const openImgPopup = (placeName, imageLink) => {
+  imgPopup.querySelector('.popup__place-img').src = imageLink;
+  imgPopup.querySelector('.popup__place-heading').textContent = placeName;
+  imgPopup.querySelector('.popup__place-img').alt = placeName;
+  openPopup(imgPopup); 
+};
